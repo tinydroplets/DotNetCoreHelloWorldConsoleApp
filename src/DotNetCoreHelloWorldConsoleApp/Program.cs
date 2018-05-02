02c394ae03f3831e3e80357fba726fa1befbb13d
tinydroplets <abdulla.mamuwala@gmail.com>
2018-05-02 08:27:42 -0500

    initial changes

---
+++ src/DotNetCoreHelloWorldConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DotNetCoreHelloWorldConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
